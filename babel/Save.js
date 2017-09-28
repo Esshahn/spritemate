27ffb0f87040701ee480380572f5ed12d0198e1f
Ingo Hinterding <ingo@hinterding.de>
2017-09-28 12:53:37 +0200

    lepros

--- babel/Save.js
+++ babel/Save.js
@@ -12,18 +12,19 @@ class Save
     <div id="window-save">
       <h1 autofocus>Save Data</h1>
       <h2>The file will be saved to your default download location</h2>
+      <br/>
       <fieldset>
         <legend>Spritemate // *.spm</legend>
         <button id="button-save-spm">Save as Spritemate *.spm</button>
         <p>The natural format for Spritemate. Recommended as long as you are not done working on the sprites.</p>
       </fieldset>
-    <!--
+    
       <fieldset>
-        <legend>Spritepad // *.spr</legend>
-        <button id="button-save">Save as Spritepad *.spr</button>
+        <legend>Spritepad // *.spd</legend>
+        <button id="button-save-spd">Save as Spritepad *.spd</button>
         <p>Most common Sprite editing software on Windows.</p>
       </fieldset>
-
+  <!--
       <fieldset>
         <legend>Binary // *.bin</legend>
         <button id="button-save">Save as Binary *.bin</button>
@@ -45,7 +46,8 @@ class Save
     $("#window-"+this.window).append(template);
     $("#window-"+this.window).dialog({ show: 'fade', hide: 'fade' });
     $('#button-save-cancel').mouseup((e) => $("#window-"+this.window).dialog( "close" ));
-    $('#button-save-spm').mouseup((e) => this.save_spm(this.savedata, 'myfilename.spm', 'text/plain'));
+    $('#button-save-spm').mouseup((e) => this.save_spm(this.savedata, 'myfilename.spm'));
+    $('#button-save-spd').mouseup((e) => this.save_spd(this.savedata, 'myfilename.spd'));
    
   }
 
@@ -54,7 +56,7 @@ class Save
   save_spm(data, filename, type)
   {
 
-      var file = new Blob([JSON.stringify(data)], {type: type});
+      var file = new Blob([JSON.stringify(data)], {type: "text/plain"});
       if (window.navigator.msSaveOrOpenBlob) // IE10+
           window.navigator.msSaveOrOpenBlob(file, filename);
       else { // Others
@@ -72,6 +74,41 @@ class Save
 
       $("#window-"+this.window).dialog( "close" );
   }
+
+  save_spd(data, filename, type)
+  {
+      var hexdata = this.create_spd_array();
+
+      var byteArray = new Uint8Array(hexdata.match(/.{2}/g).map(e => parseInt(e, 16)));
+
+      var file = new Blob([byteArray], {type: "application/octet-stream"});
+
+      if (window.navigator.msSaveOrOpenBlob) // IE10+
+          window.navigator.msSaveOrOpenBlob(file, filename);
+      else { // Others
+          var a = document.createElement("a"),
+                  url = URL.createObjectURL(file);
+          a.href = url;
+          a.download = filename;
+          document.body.appendChild(a);
+          a.click();
+          setTimeout(function() {
+              document.body.removeChild(a);
+              window.URL.revokeObjectURL(url);  
+          }, 0); 
+      }
+
+      $("#window-"+this.window).dialog( "close" );
+  }
+
+  create_spd_array()
+  {
+
+    var data = new Array(122, 222, 12, 18, 255);
+
+    return data;
+  }
+
 
   set_save_data(savedata)
   {
--- dist/js/Save.js
+++ dist/js/Save.js
@@ -13,7 +13,7 @@ var Save = function () {
     this.config = config;
     this.window = window;
 
-    var template = "\n    <div id=\"window-save\">\n      <h1 autofocus>Save Data</h1>\n      <h2>The file will be saved to your default download location</h2>\n      <fieldset>\n        <legend>Spritemate // *.spm</legend>\n        <button id=\"button-save-spm\">Save as Spritemate *.spm</button>\n        <p>The natural format for Spritemate. Recommended as long as you are not done working on the sprites.</p>\n      </fieldset>\n    <!--\n      <fieldset>\n        <legend>Spritepad // *.spr</legend>\n        <button id=\"button-save\">Save as Spritepad *.spr</button>\n        <p>Most common Sprite editing software on Windows.</p>\n      </fieldset>\n\n      <fieldset>\n        <legend>Binary // *.bin</legend>\n        <button id=\"button-save\">Save as Binary *.bin</button>\n        <p>Ready to use binary data.</p>\n      </fieldset>\n\n      <fieldset>\n        <legend>ACME Source // *.asm</legend>\n        <button id=\"button-save\">Save as ACME *.asm</button>\n        <p>Compilable source code for ACME assembler.</p>  \n      </fieldset>\n    -->\n      <div id=\"button-row\">\n        <button id=\"button-save-cancel\" class=\"button-cancel\">Cancel</button>\n      </div>\n    </div> \n    ";
+    var template = "\n    <div id=\"window-save\">\n      <h1 autofocus>Save Data</h1>\n      <h2>The file will be saved to your default download location</h2>\n      <br/>\n      <fieldset>\n        <legend>Spritemate // *.spm</legend>\n        <button id=\"button-save-spm\">Save as Spritemate *.spm</button>\n        <p>The natural format for Spritemate. Recommended as long as you are not done working on the sprites.</p>\n      </fieldset>\n    \n      <fieldset>\n        <legend>Spritepad // *.spd</legend>\n        <button id=\"button-save-spd\">Save as Spritepad *.spd</button>\n        <p>Most common Sprite editing software on Windows.</p>\n      </fieldset>\n  <!--\n      <fieldset>\n        <legend>Binary // *.bin</legend>\n        <button id=\"button-save\">Save as Binary *.bin</button>\n        <p>Ready to use binary data.</p>\n      </fieldset>\n\n      <fieldset>\n        <legend>ACME Source // *.asm</legend>\n        <button id=\"button-save\">Save as ACME *.asm</button>\n        <p>Compilable source code for ACME assembler.</p>  \n      </fieldset>\n    -->\n      <div id=\"button-row\">\n        <button id=\"button-save-cancel\" class=\"button-cancel\">Cancel</button>\n      </div>\n    </div> \n    ";
 
     $("#window-" + this.window).append(template);
     $("#window-" + this.window).dialog({ show: 'fade', hide: 'fade' });
@@ -21,7 +21,10 @@ var Save = function () {
       return $("#window-" + _this.window).dialog("close");
     });
     $('#button-save-spm').mouseup(function (e) {
-      return _this.save_spm(_this.savedata, 'myfilename.spm', 'text/plain');
+      return _this.save_spm(_this.savedata, 'myfilename.spm');
+    });
+    $('#button-save-spd').mouseup(function (e) {
+      return _this.save_spd(_this.savedata, 'myfilename.spd');
     });
   }
 
@@ -31,7 +34,7 @@ var Save = function () {
     key: "save_spm",
     value: function save_spm(data, filename, type) {
 
-      var file = new Blob([JSON.stringify(data)], { type: type });
+      var file = new Blob([JSON.stringify(data)], { type: "text/plain" });
       if (window.navigator.msSaveOrOpenBlob) // IE10+
         window.navigator.msSaveOrOpenBlob(file, filename);else {
         // Others
@@ -48,6 +51,42 @@ var Save = function () {
       }
 
       $("#window-" + this.window).dialog("close");
+    }
+  }, {
+    key: "save_spd",
+    value: function save_spd(data, filename, type) {
+      var hexdata = this.create_spd_array();
+
+      var byteArray = new Uint8Array(hexdata.match(/.{2}/g).map(function (e) {
+        return parseInt(e, 16);
+      }));
+
+      var file = new Blob([byteArray], { type: "application/octet-stream" });
+
+      if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);else {
+        // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);
+        }, 0);
+      }
+
+      $("#window-" + this.window).dialog("close");
+    }
+  }, {
+    key: "create_spd_array",
+    value: function create_spd_array() {
+
+      var data = new Array(122, 222, 12, 18, 255);
+
+      return data;
     }
   }, {
     key: "set_save_data",
