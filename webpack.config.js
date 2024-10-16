a4f413bd40a1f5cd848de784d79bc0e41f4ac3e7
Esshahn <ingo@hinterding.de>
2024-10-16 15:47:35 +0200

    updated to latest packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack"); //to access built-in plugins
-var path = require("path");
+const webpack = require("webpack");
+const path = require("path");
 
 const config = {
   entry: ["./src/js/App.ts"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Ensures the output directory is cleaned before each build
   },
   resolve: {
     extensions: [".ts", ".js"],
@@ -29,10 +30,10 @@ const config = {
         include: [path.resolve(__dirname, "src/js")],
       },
       {
-        test: /\.(jpe?g|png|gif)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "img/",
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset/resource', // Webpack's native asset handling
+        generator: {
+          filename: 'img/[hash][ext][query]', // Output images to 'img/' folder
         },
       },
       {
@@ -54,18 +55,19 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/img",
-        to: "./img",
-      },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/img"), // Source folder
+          to: path.resolve(__dirname, "dist/img"),  // Destination folder
+        },
+      ],
+    }),
   ],
+  mode: "development",
 };
 
 module.exports = config;
