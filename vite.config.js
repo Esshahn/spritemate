a0179b73f7ebc00dd3a681017cd52ead05598718
Esshahn <ingo@hinterding.de>
2025-12-27 15:12:02 +0100

    fix vite and node

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
